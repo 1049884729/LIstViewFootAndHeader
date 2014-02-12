4a399c89040f618950064ce376445216dcf8139e
xff <1049884729@qq.com>
2014-02-12 22:47:20 +0800

    service start activity after exectue a long time Task

--- ListViewFoot/src/com/example/ListViewFoot/StartAppServer.java
+++ ListViewFoot/src/com/example/ListViewFoot/StartAppServer.java
@@ -6,13 +6,16 @@ import android.content.IntentFilter;
 import android.os.Binder;
 import android.os.IBinder;
 import android.os.Looper;
+import android.util.Log;
+
+import java.util.logging.Logger;
 
 /**
  * Created by xff on 14-2-12.
  */
 public class StartAppServer extends Service {
 
-    private boolean isOnCreate=false;
+    private boolean isOnCreate=false;//线程耗时时，services执行到startCommend方法，
     public IBinder onBind(Intent intent) {
 
         return new MyIBinder();
@@ -28,17 +31,19 @@ public class StartAppServer extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
-       new initThread().start();
+
+        new initThread().start();//初始化耗时任务必须放在线程中，完成后通过广播启动完成初始化的任务
     }
 class  initThread extends Thread{
     @Override
     public void run() {
         super.run();
         try {
-            Thread.sleep(8000);
-            isOnCreate=true;
+            Thread.sleep(3000);
             Looper.prepare();
-            startMainPage();
+            startMainPage();//执行完第一次之后再改变isOnCreate的值，防止该方法执行两次
+            isOnCreate=true;
+
             Looper.loop();
 
         } catch (InterruptedException e) {
@@ -46,7 +51,8 @@ class  initThread extends Thread{
         }
     }
 }
-    void startMainPage(){
+    synchronized void startMainPage(){
+        Log.i("aaaa","dddddddddddddd");
         Intent i=new Intent();
         i.setAction("entry.to.main.broad");
         getApplicationContext().sendBroadcast(i);
